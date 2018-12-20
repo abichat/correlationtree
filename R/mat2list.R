d9827b4eefbe461c35a1dd4592b78ad8df08a2da
abichat <a.bichat@yahoo.fr>
2018-12-20 11:45:15 +0100

    First function

--- R/mat2list.R
+++ R/mat2list.R
@@ -1,11 +1,12 @@
 #' Matrix to list
-#'
+#' @description Convert a matrix of abundance to a list.
 #' @param mat matrix
-#'
 #' @return a list
 #' @export
-#'
 #' @examples
+#' m <- matrix(1:12, ncol = 3)
+#' rownames(m) <- letters[1:4]
+#' mat2list(m)
 mat2list <- function(mat){
   as.list(as.data.frame(t(mat)))
 }

ce3df720a85afb932960e2e07b49095f5c6a105e
abichat <a.bichat@yahoo.fr>
2019-01-02 11:33:13 +0100

    mat2list()

--- R/mat2list.R
+++ R/mat2list.R
@@ -1,7 +1,7 @@
 #' Matrix to list
-#' @description Convert a matrix of abundance to a list.
-#' @param mat matrix
-#' @return a list
+#' @description Convert a named matrix of abundance to a named list.
+#' @param mat matrix.
+#' @return \code{mat2list} returns a named list whose each entry is a row of the original matrix.
 #' @export
 #' @examples
 #' m <- matrix(1:12, ncol = 3)

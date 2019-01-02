f01490e938beb1381625a91c30254de3d36a2306
abichat <a.bichat@yahoo.fr>
2019-01-02 15:05:23 +0100

    cor_wo_shared_zero

---
+++ R/cor_wo_shared_zero.R
@@ -0,0 +1,16 @@
+#' Correlation without shared zeros
+#'
+#' @param x a numeric vector with positive or null entries.
+#' @param y a numeric vector with positive or null entries.
+#' @param ... arguments to be passed to \code{cor} such as \code{method} (see details).
+#' @return The estimated correlation coefficient between \code{x} and \code{y} after removing shared zeros.
+#' @export
+#' @details For \code{method}: one of "pearson" (default), "kendall", or "spearman".
+#' @examples
+#' x <- c(0, 7, 5, 0, 2)
+#' y <- c(8, 0, 0, 0, 2)
+#' cor_wo_shared_zero(x, y, method = "spearman")
+#' cor(x[-4], y[-4], method = "spearman")
+cor_wo_shared_zero <- function(x, y, ...){
+  cor(x[x != 0 | y != 0], y[x != 0 | y != 0], ...)
+}

f02f1ca92ab5acb4054107985f9bcb22114c255c
abichat <a.bichat@yahoo.fr>
2019-01-02 11:33:58 +0100

    df2mat()

---
+++ R/df2mat.R
@@ -0,0 +1,19 @@
+#' Dataframe to matrix
+#' @description Convert an abundance dataframe to a named matrix.
+#' @param df a class data.
+#' @param col integer giving the column to be passed as row names.
+#' @return \code{df2mat} returns a named matrix.
+#' @details Use \code{col = 0} if there are already row names (or use \code{as.matrix(df)} directly).
+#' @export
+#' @examples
+#' df <- data.frame(Clade = letters[1:4], S1 = 1:4, S2 = 7:8)
+#' df2mat(df)
+df2mat <- function(df, col = 1){
+  if (col == 0) {
+    return(as.matrix(df))
+  } else {
+    mat <- as.matrix(df[, -col])
+    rownames(mat) <- df[[col]]
+    return(mat)
+  }
+}

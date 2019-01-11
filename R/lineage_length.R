877e5dee5defeaa563d8530f4b6b80c810181ec4
abichat <a.bichat@yahoo.fr>
2019-01-11 17:33:14 +0100

    *lineage_length()

---
+++ R/lineage_length.R
@@ -0,0 +1,49 @@
+#' Lineage length
+#' @description Compute the distance from the root for a leaf.
+#' @param tree a "phylo" object.
+#' @param label tip label.
+#' @return A double.
+#' @export
+#' @importFrom ape read.tree
+#' @examples
+#' tree <- ape::read.tree(text = "(A:0.1,B:0.2,(C:0.3,D:0.4):0.5);")
+#' lineage_length(tree, "A")
+#' lineage_length(tree, "D")
+lineage_length <- function(tree, label){
+  N <- length(tree$tip.label)
+
+  tip <- which(tree$tip.label == label)
+  e <- which(tree$edge[, 2] == tip)
+  parent <- tree$edge[e, 1]
+  sum <- tree$edge.length[e]
+
+  while (parent > N+1) {
+    e <- which(tree$edge[, 2] == parent)
+    parent <- tree$edge[e, 1]
+    sum <- sum + tree$edge.length[e]
+  }
+  return(sum)
+}
+
+#' Mean lineage length
+#' @description Compute the mean distance from the root to leafs.
+#' @param tree a "phylo" object.
+#' @param ultrametric logical. Is \code{tree} ultrametric? (see details)
+#' @return A double.
+#' @export
+#' @importFrom ape read.tree
+#' @importFrom purrr map_dbl
+#' @details A tree is ultrametric when the distances from the root to every branch tip are equal.
+#' @examples
+#' tree <- ape::read.tree(text = "(A:0.1,B:0.2,(C:0.3,D:0.4):0.5);")
+#' lineage_length(tree, "A")
+mean_lineage_length <- function(tree, ultrametric = FALSE) {
+  if (ultrametric) {
+    L <- lineage_length(tree, tree$tip.label[1])
+    return(L)
+  } else {
+    L <- vapply(tree$tip.label, function(x) lineage_length(tree, x),
+                FUN.VALUE = double(1))
+    return(mean(L))
+  }
+}
--- tests/testthat/test-trees.R
+++ tests/testthat/test-trees.R
@@ -12,3 +12,17 @@ test_that("shuffle_tiplabels() change only tip labels",{
   expect_equal(tree2$Nnode, tree1$Nnode)
   expect_equal(sort(tree2$tip.label), sort(tree1$tip.label))
 })
+
+#### *lineage_length() ####
+
+tree3 <- read.tree(text = "(A:0.1,B:0.2,(C:0.3,D:0.4):0.5);")
+tree4 <- read.tree(text = "(A:0.9,B:0.9,(C:0.4,D:0.4):0.5);")
+
+test_that("lineage_length() are correct", {
+  expect_is(lineage_length(tree3, "A"), "numeric")
+  expect_equal(lineage_length(tree3, "A"), 0.1)
+  expect_equal(lineage_length(tree3, "C"), 0.8)
+  expect_equal(mean_lineage_length(tree3), 0.5)
+  expect_equal(mean_lineage_length(tree4), 0.9)
+  expect_equal(mean_lineage_length(tree4, ultrametric = TRUE), 0.9)
+})

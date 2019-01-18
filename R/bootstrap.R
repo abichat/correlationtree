ab1700305ab51993598cf581ee17b2ffc0d156a0
abichat <a.bichat@yahoo.fr>
2019-01-18 09:36:34 +0100

    sample_boot()

---
+++ R/bootstrap.R
@@ -0,0 +1,21 @@
+#' Variable bootstrap
+#' @description
+#' @param table a dataframe or matrix
+#' @param matrix logical
+#' @param col integer. The column to keep
+#'
+#' @return A table.
+#' @export
+#'
+#' @examples
+sample_boot <- function(table, matrix = FALSE, col = 1){
+
+  if(col == 0 | matrix){
+    newcol <- sample(ncol(table), replace = TRUE)
+  } else {
+    newcol <- c(col, sample(seq_along(table)[-col], replace = TRUE))
+  }
+
+  return(table[, newcol])
+}
+
